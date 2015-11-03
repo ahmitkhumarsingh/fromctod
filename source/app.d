ed1bfae505b3316d6106963df8126da101cdf2d5
Ahmit Singh <ahmitsingh@dispostable.com>
2015-11-03 21:00:57 +0100

    Uppercase & lowercase

--- source/app.d
+++ source/app.d
@@ -1,19 +1,8 @@
 import std.stdio;
-import std.algorithm;
+import std.uni;
 
 void main() {
 
-    int[] array = [ 1, 2, 3 ];
-
-    bool allPositive = all!"a > 0"(array);
-    bool anyZero = any!"a == 0"(array);
-    uint countPositive = count!"a > 0"(array);
-
-    writeln(allPositive);
-    writeln(anyZero);
-    writeln(countPositive);
-
-    writeln(startsWith("test", "te"));
-    writeln(endsWith("test", "st"));
-
+    writeln(toUpper("test"));
+    writeln(toLower("TEST"));
 }
