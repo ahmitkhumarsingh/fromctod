f8c105a46743f5ac7fd16397def1cf78ad4bc33c
Ahmit Singh <ahmitsingh@dispostable.com>
2015-11-03 20:47:16 +0100

    Array sorting

--- source/app.d
+++ source/app.d
@@ -1,6 +1,18 @@
 import std.stdio;
+import std.algorithm;
 
-void main()
-{
-    writeln("Hello, World!");
+void main() {
+
+    int[] array = [ 1, 3, 4, 2 ];
+
+    // sort in descending order
+    sort!("a > b")(array);
+
+    writeln(array);
+
+    // sort in ascending order
+    sort!("a < b")(array);
+
+    writeln(array);
+
 }
